c3c87a87a07dc56c79a10598bf46bf1159003d92
mack <mackcrego@gmail.com>
2024-02-06 10:14:44 +0100

    Fin seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
+//import { NgModule } from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/counter.module';
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+//import { CounterComponent } from './counter/counter.component';
+//import { HeroComponent } from './heroes/hero/hero.component';
+//import { ListComponent } from './heroes/list/list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CounterModule,
+    HeroesModule,
+   // AppRoutingModule,
+   // AppComponent
   ],
-  providers: [
+  /*providers: [
     provideClientHydration()
-  ],
+  ],*/
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
